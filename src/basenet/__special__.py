4fda74e7a5bd132bacb251571eccc01862259643
iTzAlver <ialver.p@gmail.com>
2022-10-26 18:29:16 +0200

    1.0.3

--- src/basenet/__special__.py
+++ src/basenet/__special__.py
@@ -6,10 +6,10 @@
 # - x - x - x - x - x - x - x - x - x - x - x - x - x - x - #
 import os
 __version__ = '1.0'
-__config_path__ = os.path.abspath(f'{__file__.replace(f"__special__.py", "")}/.config/config.json')
-__base_compiler__ = os.path.abspath(f'{__file__.replace(f"__special__.py", "")}/.config/'
+__config_path__ = os.path.abspath(f'{__file__.replace(f"__special__.py", "")}/include/config/config.json')
+__base_compiler__ = os.path.abspath(f'{__file__.replace(f"__special__.py", "")}/include/config/'
                                     f'compilers/base_compiler.yaml')
-__temp_path__ = os.path.abspath(f'{__file__.replace(f"__special__.py", "")}/.temp/')
+__temp_path__ = os.path.abspath(f'{__file__.replace(f"__special__.py", "")}/include/temp/')
 __keras_checkpoint__ = f'{__temp_path__}/checkpoints''/model.'
 __tensorboard_logs__ = f'{__temp_path__}/logs'
 __print_model_path__ = f'{__temp_path__}/render/'
